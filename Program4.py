30e417ff6414cea9e0ed1990727e6dd1b56e85dd
Karn <Karn.lopsoontorn@gmail.com>
2025-03-25 10:11:23 +0700

    coomit program 4

---
+++ Program4.py
@@ -0,0 +1,8 @@
+#import Program3
+from Program3 import add2, muit2
+
+print ('Insite Program4')
+
+#print ('add2(2) =', Program3.add2(3))
+print ('add2(5) =' , add2(7))
+print ('muit2(7) =', muit2(7))
