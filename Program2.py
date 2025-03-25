55fca56a2f3dfe6725c3ed4137f96af7ff88af19
Karn <Karn.lopsoontorn@gmail.com>
2025-03-25 09:56:04 +0700

    Commit Program 2

---
+++ Program2.py
@@ -0,0 +1,16 @@
+#str = 'Hellow Word'
+#print('This is a',str)
+
+
+#def funcl():
+#    print('In funcl')
+
+#print('Outsite funcl')
+
+def func2():
+    return 'In func'
+
+#funcl()
+func2()
+str2=func2()
+print(str2)
